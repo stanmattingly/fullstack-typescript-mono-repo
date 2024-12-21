7f8b3ff4d2790cc8c9b0cdfafb018c7606de6b82
Stan Mattingly <spmattingly1@gmail.com>
2024-12-21 14:02:18 -0800

    fix gha

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,24 @@
 export default {
-  projects: ['./packages/backend', './packages/frontend'],
+  projects: [
+    {
+      displayName: 'backend',
+      rootDir: './packages/backend',
+      preset: 'ts-jest', // Use ts-jest for TypeScript support
+      testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'], // Treat TypeScript files as ESM
+      transform: {
+        '^.+\\.tsx?$': ['ts-jest', { useESM: true }], // Move ts-jest config here
+      },
+    },
+    {
+      displayName: 'frontend',
+      rootDir: './packages/frontend',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom', // Use jsdom for frontend testing
+      extensionsToTreatAsEsm: ['.ts', '.tsx'],
+      transform: {
+        '^.+\\.tsx?$': ['ts-jest', { useESM: true }], // Move ts-jest config here
+      },
+    },
+  ],
 };
